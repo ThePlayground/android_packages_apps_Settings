a74ed780f945f23ef0dfdfb7e38105a48f93cebc
Twisted <twistedumbrella@gmail.com>
2012-01-08 02:13:25 -0500

    Add build date stamp (from prop ro.build.date) to About Phone in Settings
    Justin White
    http://review.cyanogenmod.com/#change,11534

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -60,7 +60,8 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment {
     private static final String KEY_FIRMWARE_VERSION = "firmware_version";
 
     private static final String KEY_MOD_VERSION = "mod_version";
-
+    private static final String KEY_MOD_BUILD_DATE = "mod_build_date";
+    
     private static final String KEY_UPDATE_SETTING = "additional_system_update_settings";
 
     long[] mHits = new long[3];
@@ -78,6 +79,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment {
         setStringSummary(KEY_BUILD_NUMBER, Build.DISPLAY);
         findPreference(KEY_KERNEL_VERSION).setSummary(getFormattedKernelVersion());
         setValueSummary(KEY_MOD_VERSION, "ro.modversion");
+        setValueSummary(KEY_MOD_BUILD_DATE, "ro.build.date");
 
         // Remove Safety information preference if PROPERTY_URL_SAFETYLEGAL is not set
         removePreferenceIfPropertyMissing(getPreferenceScreen(), "safetylegal",
